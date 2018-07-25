cce807d298052e9e8f7182166e50fb7f7c4a4e47
Denis, Makarov <makarovdenis07081985@gmail.com>
2018-07-25 20:19:32 +0300

    eleven parth

--- app/js/slick.js
+++ app/js/slick.js
@@ -13,54 +13,16 @@ nextArrow: '<button class="arrow_right" type="button"><i class="fas fa-chevron-r
 
 });
 
-$('.underslider_box_top').slick({
-  dots: false,
+
+$('.third_slider_box').slick({
+  dots: true,
   infinite: true,
-  speed: 1100,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 6000,
-  fade: true,
-});
+  speed: 3000,
+  slidesToShow: 1.08,
+  dotsClass: 'maindots',
+  arrows: true,
 
-$('.underslider_box_botton').slick({
-  dots: false,
-  infinite: true,
-  speed: 1100,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  cssEase: 'linear',
-  fade: true,
-});
+prevArrow: '<button class="third_arrow_left" type="button"><i class="fas fa-chevron-left"></i></button>',
+nextArrow: '<button class="third_arrow_right" type="button"><i class="fas fa-chevron-right"></i></button>'
 
-
-$('.our_cook_box').slick({
-  dots: false,
-  infinite: false,
-  speed: 2100,
-  slidesToShow: 1,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  cssEase: 'linear',
-});
-
-
-
-
-$('.our_cook_box_after').slick({
-  dots: false,
-  infinite: false,
-  speed: 2100,
-  slidesToShow: 1,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  cssEase: 'linear',
-  fade: true,
 });
